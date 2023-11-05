c0552014f48055cc31b63612b7a2e42b7a848676
RedMuri <celovekda@gmail.com>
2023-11-06 01:13:19 +0300

    Add delay when refreshing

--- app/src/main/java/com/example/tesths/ui/viewmodel/ProductsViewModel.kt
+++ app/src/main/java/com/example/tesths/ui/viewmodel/ProductsViewModel.kt
@@ -2,17 +2,11 @@ package com.example.tesths.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.tesths.data.Mapper
-import com.example.tesths.data.RepositoryImpl
-import com.example.tesths.data.network.ApiFactory
 import com.example.tesths.domain.usecases.GetProductsUseCase
 import com.example.tesths.ui.state.ProductsScreenState
-import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -29,11 +23,13 @@ class ProductsViewModel @Inject constructor(
         viewModelScope.launch {
             getProductsUseCase()
                 .onStart {
-                    if (shouldUpdate)
+                    if (shouldUpdate) {
                         _productsScreenState.emit(ProductsScreenState.Loading)
+                        delay(500)
+                    }
                 }
                 .collect {
-                    _productsScreenState.emit(it as ProductsScreenState)
+                    _productsScreenState.emit(it)
                 }
         }
     }
