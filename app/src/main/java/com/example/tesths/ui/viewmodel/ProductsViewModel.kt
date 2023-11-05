5cafa40c99ca09651f5c7e745142a13098643287
RedMuri <celovekda@gmail.com>
2023-11-05 13:15:43 +0300

    Add ProductsViewModel

---
+++ app/src/main/java/com/example/tesths/ui/viewmodel/ProductsViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.tesths.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tesths.domain.usecases.GetProductsUseCase
+import com.example.tesths.ui.state.ProductsScreenState
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.launch
+
+class ProductsViewModel (
+    private val getProductsUseCase: GetProductsUseCase,
+) : ViewModel() {
+
+    private val _productsScreenState =
+        MutableStateFlow<ProductsScreenState>(ProductsScreenState.Loading)
+    val productsScreenState = _productsScreenState.asStateFlow()
+
+    fun getProducts(skip: Int = 0, limit: Int = 10) {
+        viewModelScope.launch {
+            getProductsUseCase(skip, limit)
+                .catch {
+                    _productsScreenState.emit(ProductsScreenState.Error(it))
+                }.collect {
+                    _productsScreenState.emit(ProductsScreenState.Content(it))
+                }
+        }
+    }
+}
