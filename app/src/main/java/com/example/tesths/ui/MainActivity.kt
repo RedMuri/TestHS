9e2118c60d1e45765a5ab9e060d5e65c017b4092
RedMuri <celovekda@gmail.com>
2023-11-05 16:27:54 +0300

    Add injecting component in MainActivity

--- app/src/main/java/com/example/tesths/ui/MainActivity.kt
+++ app/src/main/java/com/example/tesths/ui/MainActivity.kt
@@ -3,10 +3,22 @@ package com.example.tesths.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.tesths.TestHS
 import com.example.tesths.ui.theme.TestHSTheme
+import com.example.tesths.ui.viewmodel.ViewModelFactory
+import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
+
+    private val component by lazy {
+        (application as TestHS).component
+    }
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        component.inject(this)
         super.onCreate(savedInstanceState)
         setContent {
             TestHSTheme {
